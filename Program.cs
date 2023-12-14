812f8288c7e70753f80dfd34a8f09a64a5c2284c
HemaMurugaboopathy <h.prabhaharan@gmail.com>
2023-12-14 13:06:40 +1300

    Add selflearn files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Net;
+
+IWebDriver driver = new ChromeDriver();
+driver.Navigate().GoToUrl("https://github.com/login");
+IWebElement usernameTextbox = driver.FindElement(By.XPath("//*[@id=\"login_field\"]"));
+usernameTextbox.SendKeys("h.prabhaharan@gmail.com");
+
+IWebElement passwordTextbox = driver.FindElement(By.Id("password"));
+passwordTextbox.SendKeys("June1@2008");
+
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"login\"]/div[4]/form/div/input[13]"));
+loginButton.Click();
